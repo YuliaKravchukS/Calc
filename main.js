79624408d22a41f4fc50fd4162cc78c5aab22389
YuliaKravchukS <ljuljek554@gmail.com>
2024-04-16 23:37:48 +0300

    calc

--- main.js
+++ main.js
@@ -1,192 +1,153 @@
+"use strict";
+const input = document.querySelector(".display");
+const numbers = document.querySelectorAll(".number");
+const calculate = document.querySelector(".calculate");
 
-import "./style.css";
-import * as Phaser from "phaser";
+let startValue = 0;
+let a = "";
+let b = "";
+let c = "";
+let d = "";
+let memory = "";
+let sign = "";
+let finish = false;
+input.value = startValue;
 
-let player;
-let cursors;
-let target;
-let textScore;
-let textTime;
-let timedEvent;
-let remainingTime;
-let coinMusic;
-let bombMusic;
-let bgMusic;
-let emitter;
-let bomb;
-let hitBombOccurred = false;
-let points = 0;
-const speedDown = 500;
-const playerSpeed = speedDown + 50;
-
-const sizes = {
-  width: 500,
-  height: 450,
-};
-
-const refs = {
-  gameStartDiv: document.querySelector("#gameStarDiv"),
-  gameStartBtn: document.querySelector("#gameStartBtn"),
-  gameEndDiv: document.querySelector("#gameEndDiv"),
-  gameWinLoseSpan: document.querySelector("#gameWinLoseSpan"),
-  gameEndScoreSpan: document.querySelector("#gameEndScoreSpan"),
-  gameRestartBtn: document.querySelector("#gameRestartBtn"),
-  gameSpam: document.querySelector("#gameSpam"),
-  link: document.querySelector("#link"),
-  gameResult: document.querySelector("#gameResult"),
-};
-console.log(refs.link);
-const config = {
-  type: Phaser.CANVAS,
-  width: sizes.width,
-  height: sizes.height,
-  canvas: gameCanvas,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: speedDown },
-      debug: true,
-    },
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
-  },
-};
-
-function getRandomX(x) {
-  return Math.floor(Math.random() * x);
+function clearAll() {
+  a = "";
+  b = "";
+  d = "";
+  sign = "";
 }
 
-const game = new Phaser.Game(config);
+const arrNumbers = [
+  "00",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  ".",
+];
+const arrSign = ["/", "*", "+", "-", "√", "%"];
+const arrAddSign = ["MR", "M-", "M+", "→", "MC", "AC"];
 
-function preload() {
-  this.load.image("bgt", "assets/bgt.png");
-  this.load.image("jar", "assets/jar.png");
-  this.load.image("bit1", "assets/bit1.png");
-  this.load.image("moneyd", "assets/moneyd.png");
-  this.load.image("bomb", "assets/bomb.png");
-  this.load.audio("bgDMusic", "assets/bgDMusic.mp3");
-  this.load.audio("coin", "assets/coin.mp3");
-  this.load.audio("bomb", "assets/bomb.wav");
-}
-
-function create() {
-  this.scene.pause("scene-game");
-  coinMusic = this.sound.add("coin");
-  bombMusic = this.sound.add("bomb");
-  bgMusic = this.sound.add("bgDMusic");
-  bgMusic.play();
-  //  bgMusic.stop();
-
-  this.add.image(0, 0, "bgt").setOrigin(0, 0);
-  player = this.physics.add.image(0, sizes.height, "jar");
-  player.setImmovable(true);
-  player.body.allowGravity = false;
-  player.setCollideWorldBounds(true);
-  player
-    .setSize(player.width - player.width / 4, player.height / 6)
-    .setOffset(player.width / 10, player.height - player.height / 10);
-  target = this.physics.add.image(130, 0, "bit1").setOrigin(0, 0);
-
-  target.setMaxVelocity(0, speedDown);
-  this.physics.add.overlap(target, player, getHit, null, this);
-  cursors = this.input.keyboard.createCursorKeys();
-  textScore = this.add.text(sizes.width - 110, 16, "Score: 0", {
-    fontSize: "22px Arial",
-    fill: "#000",
-  });
-  textTime = this.add.text(16, 16, "Remaining Time: 00", {
-    fontSize: "22px Arial",
-    fill: "#000",
-  });
-  timedEvent = this.time.delayedCall(30000, gameOver, [], this);
-
-  emitter = this.add.particles(0, 0, "moneyd", {
-    speed: 80,
-    gravityY: speedDown - 200,
-    scale: 0.05,
-    duration: 100,
-    emitting: false,
-  });
-  emitter.startFollow(player, player.width / 10, player.height / 20, true);
-
-  bomb = this.physics.add.image(sizes.width - 110, 0, "bomb");
-  bomb.setMaxVelocity(0, speedDown);
-
-  this.physics.add.overlap(player, bomb, hitBomb, null, this);
-
-  function getHit() {
-    coinMusic.play();
-    target.setY(0);
-    target.setX(getRandomX(400));
-    points++;
-    textScore.setText(`Score:${points}`);
-    emitter.start();
+input.addEventListener("input", () => {
+  if (input.value.length > 12) {
+    input.value = input.value.slice(0, 12);
+    console.log(input.value.length);
+    return input.value;
   }
-  function hitBomb() { 
-   
-      if (!hitBombOccurred) { 
-        bombMusic.play();
-        target.setY(0);
-        target.setX(getRandomX(600));  
-        points = Math.max(points - 1, 0); 
-        textScore.setText(`Score:${points}`);
-        hitBombOccurred = true;
-      }
-              
+});
+calculate.addEventListener("click", onCalculateClick);
+
+function onCalculateClick(e) {
+  if (e.target.nodeName !== "BUTTON") return;
+  const key = e.target.textContent;
+
+  if (key === "%") {
+    const percent = (a * b) / 100;
+    input.value = percent;
+    b = percent;
+    return;
+  }
+  if (key === "√") {
+    const sqrt = Math.sqrt(a);
+    input.value = sqrt;
+    a = sqrt;
+    return;
   }
 
-  function gameOver() {
-    this.sys.game.destroy(true);
-    if (points >= 10) {
-      refs.gameEndScoreSpan.textContent = points;
-      refs.gameWinLoseSpan.textContent = "Victory🥳!";
-      refs.gameSpam.textContent = "Please help Ukraine win! Donate👇";
-      refs.gameResult.classList.add("resultWin");
+  if (arrNumbers.includes(key)) {
+    if (b === "" && sign === "") {
+      a += key;
+      input.value = a;
+    } else if (a !== "" && b !== "" && finish) {
+      a = c;
+      b = key;
+      finish = false;
+      input.value = b;
     } else {
-      refs.gameEndScoreSpan.textContent = points;
-      refs.gameWinLoseSpan.textContent = "Lost!🙄";
-      refs.gameSpam.textContent = "Don't forget to donate👇";
-      refs.gameResult.classList.add("result");
+      b += key;
+      input.value = b;
     }
-    refs.gameEndDiv.style.display = "flex";
-    refs.link.style.display = "block";
+    return;
+  }
+
+  if (arrSign.includes(key)) {
+    sign = key;
+    input.value = sign;
+    return;
+  }
+
+  if (key === "=") {
+    switch (sign) {
+      case "/":
+        if (b === "0" || b === "00") {
+          input.value = "Error";
+          c = "";
+          return;
+        }
+        c = a / b;
+        break;
+      case "*":
+        c = a * b;
+        break;
+      case "-":
+        c = a - b;
+        break;
+      case "+":
+        c = a + b;
+        break;
+    }
+    a = c;
+    finish = true;
+    input.value = c;
+  }
+  if (arrAddSign.includes(key)) {
+    switch (key) {
+      case "MR":
+        if (memory !== "") {
+          input.value = memory;
+          a = memory;
+          b = "";
+        } else {
+          input.value = 0;
+        }
+        break;
+      case "M-":
+        if (memory !== "") {
+          memory -= c;
+        } else {
+          memory = c;
+        }
+        clearAll();
+        break;
+      case "M+":
+        if (memory !== "") {
+          memory += c;
+        } else {
+          memory = c;
+        }
+        clearAll();
+        break;
+      case "→":
+        input.value = input.value.slice(0, -1);
+        break;
+      case "MC":
+        memory = "";
+      case "AC":
+        finish = false;
+        input.value = startValue;
+        c = "";
+        clearAll();
+        memory = "";
+        break;
+    }
   }
 }
-
-function update() {
-  remainingTime = timedEvent.getRemainingSeconds();
-  textTime.setText(`Remaining Time: ${Math.round(remainingTime).toString()}`);
-  if (target.y >= sizes.height) {
-    target.setY(0);
-    target.setX(getRandomX(400));
-  }
-  if (bomb.y >= sizes.height) {
-    bomb.setY(0);
-    bomb.setX(getRandomX(600));
-  }
-  hitBombOccurred = false;
-
-  if (cursors.left.isDown) {
-    player.setVelocityX(-playerSpeed);
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(playerSpeed);
-  } else {
-    player.setVelocityX(0);
-  }
-}
-
-refs.gameStartBtn.addEventListener("click", () => {
-  refs.gameStartDiv.style.display = "none";
-  game.scene.resume("scene-game");
-});
-
-refs.gameRestartBtn.addEventListener("click", () => {
-  refs.gameStartDiv.style.display = "none";
-  refs.gameEndDiv.style.display = "none";
-  game.destroy();
-  points = 0;
-  const newGame = new Phaser.Game(config);
-});
